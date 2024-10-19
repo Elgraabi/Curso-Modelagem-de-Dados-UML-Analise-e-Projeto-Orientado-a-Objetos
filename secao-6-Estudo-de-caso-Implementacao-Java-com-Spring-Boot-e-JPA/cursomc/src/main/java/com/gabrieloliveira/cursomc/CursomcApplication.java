f118a469d6d043bd381d4d49a831b0a9b602eab7
Elgraabi <silvatdb8@gmail.com>
2024-10-19 11:54:56 -0300

    feat: Estado e cliente

--- secao-6-Estudo-de-caso-Implementacao-Java-com-Spring-Boot-e-JPA/cursomc/src/main/java/com/gabrieloliveira/cursomc/CursomcApplication.java
+++ secao-6-Estudo-de-caso-Implementacao-Java-com-Spring-Boot-e-JPA/cursomc/src/main/java/com/gabrieloliveira/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gabrieloliveira.cursomc.domain.Categoria;
+import com.gabrieloliveira.cursomc.domain.Cidade;
+import com.gabrieloliveira.cursomc.domain.Estado;
 import com.gabrieloliveira.cursomc.domain.Produto;
 import com.gabrieloliveira.cursomc.repositories.CategoriaRepository;
+import com.gabrieloliveira.cursomc.repositories.CidadeRepository;
+import com.gabrieloliveira.cursomc.repositories.EstadoRepository;
 import com.gabrieloliveira.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -35,6 +45,16 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora",2000.00);
 		Produto p3 = new Produto(null, "Mouse",80.00);
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
@@ -44,7 +64,9 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-	
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 	}
 
 }
